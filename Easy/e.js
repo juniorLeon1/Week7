88897697fb8962f887cfdc97ac6c878a893a35b6
Junior Leon <juniorleon@Administrators-MacBook-Pro.local>
2023-01-09 14:50:33 -0500

    update

---
+++ Easy/e.js
@@ -0,0 +1,3 @@
+let name1 = 'Sasuke';
+let name2 = 'Kakashi';
+console.log("The name " + name2 + " is longer than " + name1 + " by " + (name2.length-name1.length) + " character")
---
+++ VeryEasy/ve.js
@@ -0,0 +1,3 @@
+let x = 3;
+let y = 4;
+console.log("The difference between " + y + " and " + x + " is " + (y-x))
